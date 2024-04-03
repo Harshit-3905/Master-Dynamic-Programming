45cf2be3eb31a05829b14ef2b246aafb7e8d46ca
Harshit-3905 <harshitjoshileet@gmail.com>
2024-04-03 11:01:34 +0530

    Dice Combinations

---
+++ CSES/Dice_Combination.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Dice_Combination {
+    public static void main(String[] args) throws java.lang.Exception {
+        FastReader in = new FastReader();
+        int n = in.nextInt();
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = 0;
+            for (int j = 1; j <= 6; j++) {
+                if (i - j >= 0) {
+                    dp[i] = (dp[i] + dp[i - j]) % 1000000007;
+                }
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    float nextFloat() {
+        return Float.parseFloat(next());
+    }
+
+    boolean nextBoolean() {
+        return Boolean.parseBoolean(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
